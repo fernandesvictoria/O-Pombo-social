c6f91b254cfd9927da056ffb77564a133611ce4c
Victoria Fernandes <victoriafernandesgalvao@hotmail.com>
2024-08-27 14:03:34 -0300

    Initial commit

---
+++ src/main/java/com/victoria/pombo/PomboApplication.java
@@ -0,0 +1,13 @@
+package com.victoria.pombo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomboApplication.class, args);
+	}
+
+}
